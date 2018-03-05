5f74855d02584b68421b175c6824e73d2c2395e0
Julien Monnereau <jmonnereau_prestataire@gsf.fr>
2018-03-05 12:09:51 +0100

    add test

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-render(<div>Hello world from React!</div>, document.querySelector('#root'))
+import App from './App'
+
+//render(<div>Hello world from React!</div>, document.querySelector('#root'))
+render(<App />, document.getElementById('root'))
