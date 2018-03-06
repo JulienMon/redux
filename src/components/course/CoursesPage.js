a96d8ec668b3258a5f2810867fb50edac51d2446
Julien Monnereau <jmonnereau_prestataire@gsf.fr>
2018-03-06 17:47:36 +0100

    add redux

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,32 +1,30 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import * as courseActions from '../../actions/courseActions'
 
-export default class CoursesPages extends Component {
+class CoursesPages extends Component {
     state = {
         course: { title: '' }
     }
 
-    // constructor(props, context) {
-    //     super(props, context)
-
-    //     this.state = {
-    //         course: { title: '' }
-    //     }
-    // }
-
     onTitleChange = event => {
-        const course = this.state.couse
+        const course = this.state.course
         course.title = event.target.value
         this.setState({ course: course })
     }
 
     onClickSave = () => {
-        alert(`Saving ${this.state.course.title}`)
+        //alert(`Saving ${this.state.course.title}`)
+        this.props.dispatch(courseActions.createCourse(this.state.course))
     }
+
+    courseRow = (course, index) => <div key={index}>{course.title}</div>
 
     render() {
         return (
             <div>
                 <h1>Courses</h1>
+                {this.props.courses.map(this.courseRow)}
                 <h2>Add course</h2>
                 <input
                     type="text"
@@ -38,3 +36,9 @@ export default class CoursesPages extends Component {
         )
     }
 }
+
+const mapStateToProps = (state, ownProps) => ({
+    courses: state.courses
+})
+
+export default connect(mapStateToProps)(CoursesPages)
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
-import App from './components/App';
+import configureStore from './store/configureStore'
+import App from './components/App'
 
-import './styles/styles.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+const store = configureStore()
 
 //render(<div>Hello world from React!</div>, document.querySelector('#root'))
 render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
-);
+)
